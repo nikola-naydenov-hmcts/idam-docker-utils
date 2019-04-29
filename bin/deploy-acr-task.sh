29e43f460c501de62dad8526679ee5d03db1091c
Mark Ryan <mark.ryan@hmcts.net>
2019-04-29 13:35:34 +0100

    repo changes

--- bin/deploy-acr-task.sh
+++ bin/deploy-acr-task.sh
@@ -4,8 +4,8 @@ set -e
 az account set --subscription DCD-CNP-DEV
 az acr task create \
     --registry hmctspublic \
-    --name task-idam-base \
+    --name task-idam-utils \
     --file acr-build-task.yaml \
-    --context https://github.com/hmcts/idam-docker-base.git \
+    --context https://github.com/hmcts/idam-docker-utils.git \
     --branch master \
     --git-access-token $GITHUB_TOKEN
