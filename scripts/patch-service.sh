6274d40c26772ea0031c9c078dfe242bfcfcb868
Mark Ryan <mark.ryan@hmcts.net>
2019-07-01 10:48:48 +0100

    add patch service script

---
+++ scripts/patch-service.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+LABEL="${1:-citizen}"
+OPERATION="${2:-add}"
+FIELD="${3:-redirect_uri}"
+VALUE="${4:-'["https://localhost:3000/receiver"]'}"
+
+echo -e "\nService patch for:\nLabel: ${LABEL}\nOPERATION: ${OPERATION}\nFIELD: ${FIELD}\nVALUE: ${VALUE}"
+
+STATUS=$(curl -X PATCH -s -o /dev/null -w '%{http_code}' -H 'Content-Type: application/json' \
+  ${IDAM_API_URL}/testing-support/services/${LABEL} -d '[{
+  "operation": "'${OPERATION}'",
+  "field": "'${FIELD}'",
+  "value": "'${VALUE}'"
+}]')
+
+if [ $STATUS -eq 204 ]; then
+  echo "Service patched sucessfully"
+else
+  echo "ERROR: HTTPCODE = $STATUS"
+fi
