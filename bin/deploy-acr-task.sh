38bf893943fb0a8a4384433e14bd456577272613
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-05 14:48:42 +0000

    Update deploy-acr-task.sh

--- bin/deploy-acr-task.sh
+++ bin/deploy-acr-task.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-az account set --subscription DCD-CNP-DEV
+GIT_PAT=$(az keyvault secret show --vault-name infra-vault-prod --name hmcts-github-apikey --query value -o tsv)
+
 az acr task create \
     --registry hmctspublic \
     --name task-idam-utils \
     --file acr-build-task.yaml \
     --context https://github.com/hmcts/idam-docker-utils.git \
     --branch master \
-    --git-access-token $GITHUB_TOKEN
+    --git-access-token $GIT_PAT \
+    --subscription DCD-CNP-PROD
